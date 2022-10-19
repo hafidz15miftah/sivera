d31e54249d4a3919049b0c0cd1bd516a0625efae
hafidz15miftah <hafidz15miftah@gmail.com>
2022-10-19 23:54:06 +0700

    Sidebar Edit

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -34,47 +34,46 @@
             <div class="container-fluid mt-3">
                 <div class="row">
                     <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-1">
+                        <div class="card gradient-4">
                             <div class="card-body">
                                 <h3 class="card-title text-white">Baik</h3>
                                 <div class="d-inline-block">
-                                    <h2 class="text-white">4565</h2>
+                                    <h2 class="text-white">50</h2>
                                 </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-shopping-cart"></i></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-2">
-                            <div class="card-body">
-                                <h3 class="card-title text-white">Rusak Ringan</h3>
-                                <div class="d-inline-block">
-                                    <h2 class="text-white">$ 8541</h2>
-                                </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-money"></i></span>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-building"></i></span>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
                         <div class="card gradient-3">
                             <div class="card-body">
-                                <h3 class="card-title text-white">Rusak Berat</h3>
+                                <h3 class="card-title text-white">Rusak Ringan</h3>
                                 <div class="d-inline-block">
-                                    <h2 class="text-white">4565</h2>
+                                    <h2 class="text-white">4</h2>
                                 </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-users"></i></span>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-building"></i></span>
                             </div>
                         </div>
                     </div>
                     <div class="col-lg-3 col-sm-6">
-                        <div class="card gradient-4">
+                        <div class="card gradient-2">
+                            <div class="card-body">
+                                <h3 class="card-title text-white">Rusak Berat</h3>
+                                <div class="d-inline-block">
+                                    <h2 class="text-white">1</h2>
+                                </div>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-building"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-sm-6">
+                        <div class="card gradient-1">
                             <div class="card-body">
                                 <h3 class="card-title text-white">Jumlah</h3>
                                 <div class="d-inline-block">
-                                    <h2 class="text-white">99%</h2>
-                                    <p class="text-white mb-0">Jan - March 2019</p>
+                                    <h2 class="text-white">60</h2>
                                 </div>
-                                <span class="float-right display-5 opacity-5"><i class="fa fa-heart"></i></span>
+                                <span class="float-right display-5 opacity-5"><i class="fa fa-building"></i></span>
                             </div>
                         </div>
                     </div>
--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -17,12 +17,21 @@
                     <div class="form-input-content">
                         <div class="card login-form mb-0">
                             <div class="card-body pt-5 text-center">
-                                    <h4>Sistem Informasi Inventaris Barang dan Aset Desa</h4>
+                                <h4>Sistem Informasi Inventaris Barang dan Aset Desa</h4>
 
-                                <form action="/login" method="POST" class="mt-5 mb-5 login-input">
+                                <form action="{{ route('login') }}" method="POST" class="mt-5 mb-5 login-input">
                                     @csrf
+
+                                    @if($errors->any())
+                                    <div class="alert alert-danger">
+                                        @foreach ($errors->all() as $error)
+                                        {{ $error }}
+                                        @endforeach
+                                    </div>
+                                    @endif
                                     <div class="form-group">
-                                        <input type="email" class="form-control" placeholder="Email">
+                                        <input type="email" class="form-control" placeholder="Email" :value="old('email')" required autofocus>
+                                        <input-error ::messages="$errors->get('email')" class="mt-2" />
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control" placeholder="Kata Sandi">
