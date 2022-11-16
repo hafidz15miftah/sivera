06df209dfcea1354fdb34ead5b386a66e86df670
hafidz15miftah <hafidz15miftah@gmail.com>
2022-11-16 11:49:19 +0700

    Menambahkan informasi error 401 dan 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html class="h-100" lang="id">
+<title>Error 404 &minus; Sistem Informasi Inventaris Barang dan Aset Desa</title>
+
+<!-- Head -->
+@include('partials.head')
+
+<body class="h-100">
+    
+    <!-- Loading Progressbar -->
+    @include('partials.loading')
+
+    <div class="login-form-bg h-100">
+        <div class="container h-100">
+            <div class="row justify-content-center h-100">
+                <div class="col-xl-6">
+                    <div class="error-content">
+                        <div class="card mb-0">
+                            <div class="card-body text-center pt-5">
+                                <h1 class="error-text text-primary">404</h1>
+                                <h4 class="mt-4"><i class="fa fa-thumbs-down text-danger"></i> Halaman Tidak Tersedia</h4>
+                                <p>Halaman yang Anda cari tidak tersedia, pastikan alamat yang Anda masukkan benar</p>
+                                <form class="mt-5 mb-5">
+                                    
+                                    <div class="text-center mb-4 mt-4"><a href="/dashboard" class="btn btn-primary">Kembali ke Dasbor</a>
+                                    </div>
+                                </form>
+                                <div class="text-center">
+                                    <p>Hak Cipta &copy; 2022 <a href="https://kedawung-kroya.cilacapkab.go.id">Pemerintah Desa Kedawung</a></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Script -->
+    @include('partials.script')
+</body>
+</html>
