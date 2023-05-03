07d1fcd12b115623dcbdd28da4a5d6f840ecd9cf
hafidz15miftah <hafidz15miftah@gmail.com>
2023-05-03 19:12:57 +0700

    Update NavheaderLogo with SIVERA Logo and Change Headerposition to Fixed gleek.js

--- resources/views/pages/datatablepengguna.blade.php
+++ resources/views/pages/datatablepengguna.blade.php
@@ -19,6 +19,7 @@
             <div class="card">
                 <div class="card-body">
                     <h4 class="card-title">Daftar Pengguna</h4>
+                    <a class="btn btn-primary" id="tambahpengguna" style="color:white" href="javascript:void(0)"><i class="fa fa-plus"></i> Tambah Pengguna</a>
                     <div class="table-responsive">
                         <table class="table table-striped table-bordered zero-configuration">
                             <thead>
@@ -50,5 +51,7 @@
         </div>
     </div>
 </div>
+
+<!-- Modal Tambah Data Pengguna -->
 <!-- #/ container -->
 @endsection
--- resources/views/partials/navheaderlogo.blade.php
+++ resources/views/partials/navheaderlogo.blade.php
@@ -1,11 +1,11 @@
 <div class="nav-header">
     <div class="brand-logo">
         <a href="/">
-            <b class="logo-abbr"><img src="images/logo.png" alt=""> </b>
-            <span class="logo-compact"><img src="/images/logo-compact.png" alt=""></span>
-            <span class="brand-title">
-                <img src="images/logo-text.png" alt="">
-            </span>
+            <div class="row" style="text-align: center; justify-content:center; margin:-15px;">
+                <span class="brand-title"><img src="images/cilacap.png" alt="SIVERA" width="45"></span>
+                <span class="brand-title"><h2 style="color:white; margin: 8px;">SIVERA</h2></span>
+                <span class="logo-abbr"><img src="images/cilacap.png" alt="SIVERA" width="45"></span>
+            </div>
         </a>
     </div>
 </div>
